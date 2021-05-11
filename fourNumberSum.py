5f50be335c13d9cf93f28787502262590993d007
rajesh kumar <rajescse123@gmail.com>
2021-05-11 22:21:37 +0530

    four num sum to target sum

---
+++ fourNumberSum.py
@@ -0,0 +1,30 @@
+'''
+i/p : [7, 6, 4, -1, 1, 2]
+targetSum : 16
+o/p : [
+  [7, 6, 4, -1],
+  [7, 6, 1, 2]
+]
+'''
+
+
+
+
+def fourNumberSum(array, targetSum):
+    # Write your code here.
+	allPairSums={}
+	quadruplets=[]
+	for i in range(1,len(array)-1):
+		for j in range(i+1,len(array)):
+			currentSum = array[i] + array[j]
+			difference = targetSum - currentSum
+			if difference in allPairSums:
+				for pair in allPairSums[difference]:
+					quadruplets.append(pair + [array[i],array[j]])
+		for k in range(0,i):
+			currentSum = array[i] + array[k]
+			if currentSum not in allPairSums:
+				allPairSums[currentSum]=[[array[k],array[i]]]
+			else:
+				allPairSums[currentSum].append([array[k],array[i]])
+	return quadruplets	
