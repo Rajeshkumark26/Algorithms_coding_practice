24df1fe41f962316c267ab1b20737d72a6dfcf56
rajesh kumar <rajescse123@gmail.com>
2021-05-15 19:55:18 +0530

    longest common subsequence of string

---
+++ longestCommonSubsequence.py
@@ -0,0 +1,18 @@
+'''
+i/p : str1="ZXVVYZW"
+      str2="XKYKZPW"  
+o/p : ["X", "Y", "Z", "W"]
+
+'''
+
+
+
+def longestCommonSubsequence(str1, str2):
+    lcs=[[[] for x in range(len(str1)+1)] for y in range(len(str2) + 1)]
+	for i in range(1,len(str2) + 1):
+		for j in range(1,len(str1)+1):
+			if str2[i-1]==str1[j-1]:
+				lcs[i][j]=lcs[i-1][j-1] + [str2[i-1]]
+			else:
+				lcs[i][j]=max(lcs[i-1][j],lcs[i][j-1],key=len)
+	return lcs[-1][-1]
